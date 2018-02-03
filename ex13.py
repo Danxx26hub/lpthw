0976486a3b32da32ee878ee8be81dd44e9244d4b
Daniel <danxx26@gmail.com>
2018-02-02 22:30:30 -0800

    added ex13 which uses argv from sys module

---
+++ ex13.py
@@ -0,0 +1,16 @@
+#!/Library/Frameworks/Python.framework/Versions/3.6/bin/python3
+
+from sys import argv
+
+
+script, first, second, third = argv
+
+print("the script is called:", script)
+
+print("your first variable is:", first)
+
+print("your second variable is:", second)
+
+print("your third variable is:", third)
+
+
