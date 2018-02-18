7e9d8f65364478caad197dcc482a60f9dc250799
Daniel <danxx26@gmail.com>
2018-02-18 10:12:57 -0800

    added loop for bytecode

--- sock.py
+++ sock.py
@@ -4,4 +4,28 @@ import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-print(s)
+# print(s)
+
+server = 'ibarinc.com'
+port = 80
+
+server_ip = socket.gethostbyname(server)
+print(server_ip)
+
+request = "GET /HTTP/1.1\nHost:"+server+"\n\n"
+
+s.connect((server,port))
+
+s.send(request.encode())
+
+
+results = s.recv(4096)
+
+# print(results)
+
+while (len(results)) > 0:
+    print(results)
+
+    results = s. recv(4096)
+
+
